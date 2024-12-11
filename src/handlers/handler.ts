f3a1ef4dbbaad2514f58183c7dce25a9e7ecf1e5
Andrel Lira <andrellira85@gmail.com>
2024-12-11 16:33:58 +0000

    hello function

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -3,6 +3,10 @@ import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const hello = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Acess-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       message: 'Go Serverless v4.0! Your function executed successfully!',
     }),
